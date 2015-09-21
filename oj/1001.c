7dba8390fddf9599efb839f797aae94584121bb1
zmm2015 <1397340582@qq.com>
2015-09-21 21:54:33 +0800

    Update 1001.c

--- oj/1001.c
+++ oj/1001.c
@@ -3,11 +3,11 @@
 int main(int argc, char const *argv[])
 {
 	long long n;
-	while(scanf("%ld",&n)!=EOF)
+	while(scanf("%lld",&n)!=EOF)
 	{
 		long long sum=0;
 		sum=(n+1)*n/2;
-		printf("%ld\n\n", sum);
+		printf("%lld\n\n", sum);
 	}
 	return 0;
 }
